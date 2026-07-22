28931900aa4d16de11826b3a524cf78cd184569d
KHJ <000304jun@naver.com>
2026-07-22 22:12:00 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 2,740 ms, Memory: 78,080 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.py
@@ -10,49 +10,52 @@ def check():
                     break
             else:
                 cnt = 1
-                
         if not passed:
             return False
     return True
 
-def dfs(row, cnt, limit):
+
+def dfs(y_index, cnt, limit):
     global passed
     
+    # 검수 기준 통과 시 종료
     if passed:
         return
     
+    # 정해놓은 한도에 색칠 횟수가 도달 했을 때 check하고 합격 시 종료
     if cnt == limit:
         if check():
             passed = True
         return
         
-    if row == D:
+    # 검수에 합격하지 못한 채로 y 인덱스의 끝까지 도달했을 때 종료(실패)
+    if y_index == D:
         return
     
-    # 1. 그냥 계속 진행
-    dfs(row+1, cnt, limit)
+    # 3가지 분기
     
-    # 2. 0으로 투약
-    origin_row = grid[row][:]
-    grid[row] = [0] * W
-    dfs(row+1, cnt+1, limit)
+    # 1. 그냥 다음 진행
+    dfs(y_index+1, cnt, limit)
     
-    # 3. 1로 투약
-    grid[row] = [1] * W
-    dfs(row+1, cnt+1, limit)
+    # 2. 0으로 색칠
+    origin_row = grid[y_index][:]
+    grid[y_index] = [0] * W
+    dfs(y_index+1, cnt+1, limit)
     
-    grid[row] = origin_row
+    # 3. 1로 색칠
+    grid[y_index] = [1] * W
+    dfs(y_index+1, cnt+1, limit)
     
-
-
+    # 원상복구
+    grid[y_index] = origin_row
 
 T = int(input())
 for tc in range(1, T+1):
-    D, W, K = map(int, input().split())     # D는 두께(y좌표), W는 가로길이 (x좌표), K는 검수 기준
+    D, W, K = map(int, input().split())     # D는 두께 (y좌표), W는 가로 크기(x좌표), K는 검수 기준
     grid = [list(map(int, input().split())) for _ in range(D)]
     
     if K == 1 or check():
-        print(f"#{tc} 0")
+        print(f"#{tc} {0}")
         continue
     
     passed = False
@@ -62,7 +65,5 @@ for tc in range(1, T+1):
         if passed:
             print(f"#{tc} {i}")
             break
-    
-        
         
     
